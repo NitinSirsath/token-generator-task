71e1f6cbbbcb2e23a8ab32ae724f9ac7dda85d59
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2023-01-13 14:27:45 +0530

    manage state
    state management and added generator and clear button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,65 @@
+import { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { styled } from "@mui/system";
+import Button from '@mui/material/Button';
 
-import './App.css'
-import InputField from './components/InputField'
+const InputWrapper = styled("div")({
+  color: "darkslategray",
+  // backgroundColor: 'aliceblue',
+  display: "flex",
+  justifyContent: "center",
+});
+
+const ButtonContainer = styled('div')({
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '10px',
+  marginTop: '30px',
+ });
 
 function App() {
-
-
+  const [blueTokenNumberValue, setBlueTokenNumberValue] = useState();
+  const [blueTokenRow, setBlueTokenRow] = useState();
+  const [blueTokenPrefix, setBlueTokenPrefix] = useState('');
+  const [redTokenNumberValue, setRedTokenNumberValue] = useState();
+  const [redTokenRow, setRedTokenRow] = useState();
+  const [redTokenPrefix, setRedTokenPrefix] = useState('');
+ 
+  
   return (
     <div className="App">
-    <h1>Token Generator Application</h1>
-    <div>
-    <InputField label='Number of blue tokens'/>
-    <InputField label='Number of red tokens'/>
+      <h1>Token Generator Application</h1>
+      <InputWrapper>
+        <InputField
+          TokenNumberlabel="Number of Blue tokens"
+          TokenPrefixLabel="Blue Token Prefix"
+          TokenPerRowLabel="Blue Token Per Row"
+          TokenNumberValue={blueTokenNumberValue}
+          setTokenNumberValue={setBlueTokenNumberValue}
+          TokenRow={blueTokenRow} 
+          setTokenRow={setBlueTokenRow}
+          TokenPrefix={blueTokenPrefix} 
+          setTokenPrefix={setBlueTokenPrefix}
+        />
+        <InputField
+          TokenNumberlabel="Number of Red tokens"
+          TokenPrefixLabel="Red Token Prefix"
+          TokenPerRowLabel="Red Token Per Row"
+          TokenNumberValue={redTokenNumberValue}
+          setTokenNumberValue={setRedTokenNumberValue}
+          TokenRow={redTokenRow}
+          setTokenRow={setRedTokenRow}
+          TokenPrefix={redTokenPrefix}
+          setTokenPrefix={setRedTokenPrefix}
+        />
+      </InputWrapper>
+        <ButtonContainer>
+        <Button variant="contained">Gernerate Token</Button>
+        <Button variant="outlined">Clear</Button>
+        </ButtonContainer>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { styled } from '@mui/system';
+
+const Container = styled(Box)({
+ display: 'flex',
+    flexDirection: 'column',
+});
 
 interface InputFieldProps {
-    label: string;
+    TokenNumberlabel: string,
+    TokenPerRowLabel: string,
+    TokenPrefixLabel: string,
+    TokenNumberValue: any,
+    setTokenNumberValue: any,
+    TokenRow: any,
+    setTokenRow: any,
+    TokenPrefix: any,
+    setTokenPrefix: any,
 }
 
-const InputField = ({label}:InputFieldProps) => {
+const InputField = ({TokenNumberlabel,TokenPrefixLabel,TokenPerRowLabel,TokenNumberValue,setTokenNumberValue,TokenRow,setTokenRow,TokenPrefix,setTokenPrefix}:InputFieldProps) => {
+  
+    
   return (
-    <Box
+    <Container
     component="form"
     sx={{
       '& .MuiTextField-root': { m: 1, width: '25ch' },
@@ -18,13 +34,35 @@ const InputField = ({label}:InputFieldProps) => {
   >
      <TextField
           id="outlined-number"
-          label={label}
+          label={TokenNumberlabel}
           type="number"
+          value={TokenNumberValue}
+            onChange={(e) => setTokenNumberValue(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
         />
-    </Box>
+     <TextField
+          id="outlined-number"
+          label={TokenPrefixLabel}
+          type="char"
+          value={TokenPrefix}
+            onChange={(e) => setTokenPrefix(e.target.value)}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+     <TextField
+          id="outlined-number"
+          label={TokenPerRowLabel}
+          type="number"
+            value={TokenRow}
+            onChange={(e) => setTokenRow(e.target.value)}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+    </Container>
   )
 }
 
